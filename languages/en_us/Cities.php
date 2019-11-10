fd3c5268893ca357ac4d2000b0270c5e760cd988
Sergey <sergey86@gmail.com>
2019-11-10 20:45:00 +0300

    PT186 - module Cities

---
+++ languages/en_us/Cities.php
@@ -0,0 +1,25 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+  'Cities' => 'Cities',
+  'SINGLE_Cities' => 'City',
+  'Cities ID' => 'City ID',
+  'LBL_CITIES_INFORMATION' => 'City Information',
+  'Created Time' => 'Created At',
+  'Modified Time' => 'Modified At',
+  'Modified By' => 'Modified By',
+  'LBL_DESCRIPTION' => 'Description',
+  'LBL_ADD_RECORD' => 'Add City',
+);
+
+$jsLanguageStrings = array (
+);
---
+++ languages/ru_ru/Cities.php
@@ -0,0 +1,25 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Дополнительная информация',
+  'Cities' => 'Города',
+  'SINGLE_Cities' => 'Город',
+  'Cities ID' => 'ID города',
+  'LBL_CITIES_INFORMATION' => 'Информация о городе',
+  'Created Time' => 'Дата создания',
+  'Modified Time' => 'Дата изменения',
+  'Modified By' => 'Кем изменён',
+  'LBL_DESCRIPTION' => 'Описание',
+  'LBL_ADD_RECORD' => 'Добавить город',
+);
+
+$jsLanguageStrings = array (
+);
