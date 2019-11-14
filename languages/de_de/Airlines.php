04a30a7b5874a3b5cf8dacdd186187b616569e0c
Sergey <sergey86@gmail.com>
2019-11-14 21:55:09 +0300

    PT194 - Created Airlines module

---
+++ languages/de_de/Airlines.php
@@ -0,0 +1,25 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+  'Airlines' => 'Airlines',
+  'SINGLE_Airlines' => 'Airline',
+  'Airlines ID' => 'Airline ID',
+  'LBL_AIRLINES_INFORMATION' => 'Airline Information',
+  'Created Time' => 'Created At',
+  'Modified Time' => 'Modified At',
+  'Modified By' => 'Modified By',
+  'LBL_DESCRIPTION' => 'Description',
+  'LBL_ADD_RECORD' => 'Add Airline',
+);
+
+$jsLanguageStrings = array (
+);
