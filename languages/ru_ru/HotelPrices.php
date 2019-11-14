5d6521e951dc006841c7740cb2622fe38eb14713
Sergey <sergey86@gmail.com>
2019-11-14 20:54:01 +0300

    PT192 - created HotelPrices module

---
+++ languages/ru_ru/HotelPrices.php
@@ -0,0 +1,25 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Дополнительная информация',
+  'HotelPrices' => 'Цены на отели',
+  'SINGLE_HotelPrices' => 'Цена на отель',
+  'HotelPrices ID' => 'ID цены',
+  'LBL_HOTELPRICES_INFORMATION' => 'Информация о цене на отель',
+  'Created Time' => 'Дата создания',
+  'Modified Time' => 'Дата изменения',
+  'Modified By' => 'Кем изменено',
+  'LBL_DESCRIPTION' => 'Описание',
+  'LBL_ADD_RECORD' => 'Добавить цену на отель',
+);
+
+$jsLanguageStrings = array (
+);
