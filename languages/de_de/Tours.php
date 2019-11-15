fdc89e5d3f295d5a09758dc441f5025575376aa5
Sergey <sergey86@gmail.com>
2019-11-15 21:40:26 +0300

    PT195 - created Tours module

---
+++ languages/de_de/Tours.php
@@ -0,0 +1,25 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+  'Tours' => 'Tours',
+  'SINGLE_Tours' => 'Tour',
+  'Tours ID' => 'Tour ID',
+  'LBL_TOURS_INFORMATION' => 'Tour Information',
+  'Created Time' => 'Created At',
+  'Modified Time' => 'Modified At',
+  'Modified By' => 'Modified By',
+  'LBL_DESCRIPTION' => 'Description',
+  'LBL_ADD_RECORD' => 'Add Tour',
+);
+
+$jsLanguageStrings = array (
+);
---
+++ languages/ru_ru/Tours.php
@@ -0,0 +1,29 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Дополнительная информация',
+  'Tours' => 'Туры',
+  'SINGLE_Tours' => 'Тур',
+  'Tours ID' => 'ID тура',
+  'LBL_TOURS_INFORMATION' => 'Информация о туре',
+  'Created Time' => 'Дата создания',
+  'Modified Time' => 'Дата изменения',
+  'Modified By' => 'Кем изменено',
+  'LBL_DESCRIPTION' => 'Описание',
+  'LBL_ADD_RECORD' => 'Добавить тур',
+  'Qty' => 'Количество человек',
+  'Date start' => 'Дата начала',
+  'Date end' => 'Дата окончания',
+  'Profit' => 'Прцент наценки'
+);
+
+$jsLanguageStrings = array (
+);
