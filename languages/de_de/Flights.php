a181cd2d12d4947bdf495cff613ea14cef04045b
Sergey <sergey86@gmail.com>
2019-11-15 21:54:26 +0300

    PT196 - created Flights module

---
+++ languages/de_de/Flights.php
@@ -0,0 +1,25 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+  'Flights' => 'Flights',
+  'SINGLE_Flights' => 'Flight',
+  'Flights ID' => 'Flight ID',
+  'LBL_FLIGHTS_INFORMATION' => 'Flight Information',
+  'Created Time' => 'Created At',
+  'Modified Time' => 'Modified At',
+  'Modified By' => 'Modified By',
+  'LBL_DESCRIPTION' => 'Description',
+  'LBL_ADD_RECORD' => 'Add Flight',
+);
+
+$jsLanguageStrings = array (
+);
