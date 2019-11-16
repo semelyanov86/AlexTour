3d497aef30b4ad5bb542d07992c660fa5edc5eca
Sergey <sergey86@gmail.com>
2019-11-16 15:40:34 +0300

    PT198 - created HotelArrivals module

---
+++ languages/ru_ru/HotelArrivals.php
@@ -0,0 +1,32 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Дополнительная информация',
+  'HotelArrivals' => 'Занятость в отелях',
+  'SINGLE_HotelArrivals' => 'Занятость',
+  'HotelArrivals ID' => 'ID занятости',
+  'LBL_HOTELARRIVALS_INFORMATION' => 'Информация о занятости в отеле',
+  'Created Time' => 'Дата создания',
+  'Modified Time' => 'Дата изменения',
+  'Modified By' => 'Кем изменено',
+  'LBL_DESCRIPTION' => 'Описание',
+  'LBL_ADD_RECORD' => 'Добавить занятость',
+  'Date In' => 'Дата заезда',
+  'Date Out' => 'Дата выезда',
+  'Rooms' => 'Количество комнат',
+  'Roomnights' => 'Количество ночей',
+  'Pax' => 'Количество проживающих',
+  'Price' => 'Стоимость проживания',
+  'Breakfast Box' => 'Breakfast box before 07:00'
+);
+
+$jsLanguageStrings = array (
+);
