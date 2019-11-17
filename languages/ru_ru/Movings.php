c0ca2b49312bf5942c16c06ed5eb9caaf2753788
Sergey <sergey86@gmail.com>
2019-11-17 18:45:44 +0300

    PT203 - created movings module

---
+++ languages/ru_ru/Movings.php
@@ -0,0 +1,32 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Дополнительная информация',
+  'Movings' => 'Перелёты',
+  'SINGLE_Movings' => 'Перелёт',
+  'Movings ID' => 'ID перелёта',
+  'LBL_MOVINGS_INFORMATION' => 'Информация о перелёте',
+  'Created Time' => 'Дата создания',
+  'Modified Time' => 'Дата изменения',
+  'Modified By' => 'Кем изменено',
+  'LBL_DESCRIPTION' => 'Описание',
+  'LBL_ADD_RECORD' => 'Добавить перемещение',
+  'MType' => 'Куда',
+  'In' => 'Туда',
+  'Out' => 'Обрбатно',
+  'Through' => 'Через',
+  'Date of Flight' => 'Дата полёта',
+  'Departure time' => 'Время вылета',
+    'Arrival time' => 'Время прилёта'
+);
+
+$jsLanguageStrings = array (
+);
