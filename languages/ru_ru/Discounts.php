4aa2ca15da81a4ea0f783dc8b1b12945769f5aa1
Sergey <sergey86@gmail.com>
2019-11-21 22:11:56 +0300

    PT207 - created Discounts module

---
+++ languages/ru_ru/Discounts.php
@@ -0,0 +1,27 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Дополнительная информация',
+  'Discounts' => 'Скидки',
+  'SINGLE_Discounts' => 'Скидка',
+  'Discounts ID' => 'ID скидки',
+  'LBL_DISCOUNTS_INFORMATION' => 'Информация о скидки',
+  'Created Time' => 'Дата создания',
+  'Modified Time' => 'Дата изменения',
+  'Modified By' => 'Кем изменено',
+  'LBL_DESCRIPTION' => 'Описание',
+  'LBL_ADD_RECORD' => 'Добавить скидку',
+  'Quantity' => 'Количество',
+  'Price' => 'Размер скидки',
+);
+
+$jsLanguageStrings = array (
+);
