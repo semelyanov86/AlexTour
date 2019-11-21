30cbe97da09cec39c5603de9d6b45f41405be598
Sergey <sergey86@gmail.com>
2019-11-21 21:07:20 +0300

    PT206 - TourPrices module

---
+++ languages/ru_ru/TourPrices.php
@@ -0,0 +1,30 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Дополнительная информация',
+  'TourPrices' => 'Цены на туры',
+  'SINGLE_TourPrices' => 'Цена на тур',
+  'TourPrices ID' => 'ID цены на тур',
+  'LBL_TOURPRICES_INFORMATION' => 'Информация о цене на тур',
+  'Created Time' => 'Дата создания',
+  'Modified Time' => 'Дата изменения',
+  'Modified By' => 'Кем изменено',
+  'LBL_DESCRIPTION' => 'Описание',
+  'LBL_ADD_RECORD' => 'Добавить цену на тур',
+  'Date Start' => 'Дата начала',
+  'Date End' => 'Дата окончания',
+  'Status' => 'Статус',
+  'Price Per Person' => 'Цена',
+  'EZZ' => 'EZZ',
+);
+
+$jsLanguageStrings = array (
+);
