2116f545656ef5bbec49ee11e1e714d6cef172ca
Sergey <sergey86@gmail.com>
2019-11-22 21:08:47 +0300

    PT208 - TourServices module

---
+++ languages/ru_ru/TourServices.php
@@ -0,0 +1,26 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Дополнительная информация',
+  'TourServices' => 'Туруслуги',
+  'SINGLE_TourServices' => 'Туруслуга',
+  'TourServices ID' => 'ID туристической услуги',
+  'LBL_TOURSERVICES_INFORMATION' => 'Информация об услуге',
+  'Created Time' => 'Дата создания',
+  'Modified Time' => 'Дата изменения',
+  'Modified By' => 'Кем изменено',
+  'LBL_DESCRIPTION' => 'Описание',
+  'LBL_ADD_RECORD' => 'Добавить туруслугу',
+  'Name' => 'Наименование',
+);
+
+$jsLanguageStrings = array (
+);
