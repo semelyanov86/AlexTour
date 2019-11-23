196333ee5436b9874f0b1f5d89aea747f51c1b0f
Sergey <sergey86@gmail.com>
2019-11-23 21:20:32 +0300

    PT212 - created ServiceRequests module

---
+++ languages/en_us/ServiceRequests.php
@@ -0,0 +1,25 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+  'ServiceRequests' => 'ServiceRequests',
+  'SINGLE_ServiceRequests' => 'ServiceRequest',
+  'ServiceRequests ID' => 'ServiceRequest ID',
+  'LBL_SERVICEREQUESTS_INFORMATION' => 'ServiceRequest Information',
+  'Created Time' => 'Created At',
+  'Modified Time' => 'Modified At',
+  'Modified By' => 'Modified By',
+  'LBL_DESCRIPTION' => 'Description',
+  'LBL_ADD_RECORD' => 'Add ServiceRequest',
+);
+
+$jsLanguageStrings = array (
+);
---
+++ languages/ru_ru/ServiceRequests.php
@@ -0,0 +1,32 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Дополнительная информация',
+  'ServiceRequests' => 'Запросы услуг',
+  'SINGLE_ServiceRequests' => 'Запрос услуги',
+  'ServiceRequests ID' => 'ID запроса услуги',
+  'LBL_SERVICEREQUESTS_INFORMATION' => 'Информация по запросам',
+  'Created Time' => 'Дата создания',
+  'Modified Time' => 'Дата изменения',
+  'Modified By' => 'Кем изменено',
+  'LBL_DESCRIPTION' => 'Описание',
+  'LBL_ADD_RECORD' => 'Добавить запрос по услуге',
+  'Name' => 'Наименование',
+  'Service Type' => 'Тип услуги',
+  'Additional Bed' => 'Дополнительная кровать',
+  'Status' => 'Статус',
+  'Created' => 'Создано',
+  'In progress' => 'На рассмотрении',
+  'Approved' => 'Одобрено',
+);
+
+$jsLanguageStrings = array (
+);
