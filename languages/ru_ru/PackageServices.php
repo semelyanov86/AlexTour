236135a57f0e4d416e86cc448627e46cd756c0ec
Sergey <sergey86@gmail.com>
2019-12-07 19:22:09 +0300

    PT216 - installed PackageServices module

---
+++ languages/ru_ru/PackageServices.php
@@ -0,0 +1,33 @@
+<?php
+/*+**********************************************************************************
+ * The contents of this file are subject to the vtiger CRM Public License Version 1.0
+ * ("License"); You may not use this file except in compliance with the License
+ * The Original Code is:  vtiger CRM Open Source
+ * The Initial Developer of the Original Code is vtiger.
+ * Portions created by vtiger are Copyright (C) vtiger.
+ * All Rights Reserved.
+ ************************************************************************************/
+
+$languageStrings = array (
+  'LBL_CUSTOM_INFORMATION' => 'Дополнительная информация',
+  'PackageServices' => 'Услуги к пакету',
+  'SINGLE_PackageServices' => 'Услуга к пакету',
+  'PackageServices ID' => 'ID услуги к пакету',
+  'LBL_PACKAGESERVICES_INFORMATION' => 'Информация об услуге к пакету',
+  'Created Time' => 'Дата создания',
+  'Modified Time' => 'Дата изменения',
+  'Modified By' => 'Кем изменено',
+  'LBL_DESCRIPTION' => 'Описание',
+  'LBL_ADD_RECORD' => 'Добавить услугу к пакету',
+  'Double Room' => 'Двухместный номер',
+  'One Room' => 'Одноместный номер',
+  'Trip' => 'Экскурсия',
+  'Vegetarian Meal' => 'Вегетарианское питание',
+  'Transfer' => 'Трансфер',
+  'Visa' => 'Виза',
+  'Insurance' => 'Страховка',
+  'Additional Bed' => 'Дополнительная кровать',
+);
+
+$jsLanguageStrings = array (
+);
